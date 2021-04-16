f502e3bf3bcd5aad5e8e2efdeee0a457067aa0cf
J-H-Freedman <jacob@jhfreedman.com>
2021-04-16 16:49:54 -0400

    Do nav Menu and first draft program

--- 21-04-13_assignment3/src/DriverExam.java
+++ 21-04-13_assignment3/src/DriverExam.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class DriverExam {
      // the correct answer constant to be checked against
-    char[] CORRECT_ANSWERS = {
+    static char[] CORRECT_ANSWERS = {
             'B', 'D', 'A', 'A', 'C',
             'A', 'B', 'A', 'C', 'D',
             'B', 'C', 'D', 'A', 'D',
@@ -10,12 +10,26 @@ public class DriverExam {
     };
 
     // the user's stored answers
-    char[] userAnswers = new char[20];
+    char[] userAnswers = new char[CORRECT_ANSWERS.length];
 
     // the error message that appears when there is a validation error
-    String invalidErrorMessage = "This answer is invalid. Please try again...";
+    String invalidErrorMessage = "This input is invalid. Please try again...";
 
-    public char getCORRECT_ANSWER(int i) {
+    // which question we are currently on
+    int navTracker = 0;
+
+    // is the exam completed?
+    boolean examCompleted = false;
+
+    // keyborad input
+    Scanner keyboard = new Scanner(System.in);
+
+    /**
+     * get a specific correct answer
+     * @param i the index of the answer
+     * @return the answer for the index inq question
+     */
+    public static char getCORRECT_ANSWER(int i) {
         return CORRECT_ANSWERS[i];
     }
 
@@ -34,7 +48,7 @@ public class DriverExam {
      * @param answer the answer to be set
      */
     public void addUserAnswer(int i, char answer) {
-        this.userAnswers[i] = answer;
+        this.userAnswers[i] = Character.toUpperCase(answer);
     }
 
     /**
@@ -56,7 +70,7 @@ public class DriverExam {
         char userInput;
         // validate before continuing
         do {
-            userInput = keyboard.next().charAt(0);
+            userInput = Character.toUpperCase(keyboard.next().charAt(0));
             if (isValid(userInput)) {
                 readyForNext = true;
             }
@@ -69,18 +83,122 @@ public class DriverExam {
     }
 
     /**
-     * Takes input from user and places it in the examAnswers[][1] column until through to the end.
+     * Navigate which question will be asked next
+     * @param i the input to decide where to go next
      */
-    public void fullExam() {
-        System.out.println("The exam has begun.");
-        for (int questionNumber = 1; questionNumber < CORRECT_ANSWERS.length; questionNumber++) {
-            System.out.println("Enter the answer to Question " + questionNumber);
-            answerQuestion(questionNumber);
+    void navigate(int i) {
+        if (1 <= i && i <= CORRECT_ANSWERS.length) {
+            navTracker = i-1;
         }
-        System.out.println("The exam is now complete. Thank you.");
+        else if (i == -1) {
+            navTracker--;
+        }
+        else if (i == 0) {
+            navTracker++;
+        }
+    }
+
+    /**
+     * Navigate to previous question
+     */
+    void navPrev() {
+        navigate(-1);
+    }
+
+    /**
+     * Navigate to next question
+     */
+    void navNext(){
+        navigate(0);
+    }
+
+    /**
+     * Select which question to go to next
+     */
+    void navSelect() {
+        boolean selectionCompleted = false;
+        System.out.println("Please select which question you would like to navigate to:");
+        do {
+            int i = keyboard.nextInt();
+            if (1 <= i && i <= CORRECT_ANSWERS.length) {
+                navigate(i);
+                selectionCompleted = true;
+            }
+            else {
+                System.out.println(invalidErrorMessage);
+            }
+        } while (!selectionCompleted);
+    }
+
+    /**
+     * Based con context, choose to go previous, next, select, or finish
+     */
+    void navMenu() {
+        boolean navCompleted = false;
+        if (navTracker == 0) {
+            do {
+                System.out.println("Type \"next\" or \"select\"");
+                String navInput = keyboard.nextLine().toLowerCase();
+                switch (navInput) {
+                    case "next" -> {
+                        navNext();
+                        navCompleted = true;
+                    }
+                    case "select" -> {
+                        navSelect();
+                        navCompleted = true;
+                    }
+                    default -> System.out.println(invalidErrorMessage);
+                }
+            } while (!navCompleted);
+        }
+        else if (navTracker == (DriverExam.CORRECT_ANSWERS.length-1)) {
+            do {
+                System.out.println("Type \"prev,\" \"select,\" or \"finish\"");
+                String navInput = keyboard.nextLine().toLowerCase();
+                switch (navInput) {
+                    case "prev" -> {
+                        navPrev();
+                        navCompleted = true;
+                    }
+                    case "finish" -> {
+                        navCompleted = true;
+                        examCompleted = true;
+                    }
+                    case "select" -> {
+                        navSelect();
+                        navCompleted = true;
+                    }
+                    default -> System.out.println(invalidErrorMessage);
+                }
+            } while (!navCompleted);
+        }
+        else {
+            do {
+                System.out.println("Type \"prev,\" \"next,\" or \"select\"");
+                String navInput = keyboard.nextLine().toLowerCase();
+                switch (navInput) {
+                    case "prev" -> {
+                        navPrev();
+                        navCompleted = true;
+                    }
+                    case "next" -> {
+                        navNext();
+                        navCompleted = true;
+                    }
+                    case "select" -> {
+                        navSelect();
+                        navCompleted = true;
+                    }
+                    default -> System.out.println(invalidErrorMessage);
+                }
+            } while (!navCompleted);
+        }
     }
 
     /* how many correct
+    }
+
     create counter
     for i in examAnswers.length
         if examAnswers[i][0]==examAnswers[i][1]
@@ -100,4 +218,23 @@ public class DriverExam {
         if examAnswers[i][1] == null
             counter++
     return counter */
+
+    public static void main(String[] args) {
+        DriverExam test = new DriverExam();
+        char[] testAnswers = {
+                'A', 'B', 'C', 'D', 'E',
+                'a', 'b', 'c', 'd', 'e', ' '
+         };
+
+        for (int i=0; i<CORRECT_ANSWERS.length; i++) {
+            System.out.print(getCORRECT_ANSWER(i));
+        }
+
+        for (int i=0; i<testAnswers.length; i++) {
+            test.addUserAnswer(i, testAnswers[i]);
+            System.out.print(test.getUserAnswer(i));
+            System.out.println(test.isValid(test.userAnswers[i]));
+        }
+    }
 }
+
--- 21-04-13_assignment3/src/DriverExamDemo.java
+++ 21-04-13_assignment3/src/DriverExamDemo.java
@@ -1,2 +1,13 @@
 public class DriverExamDemo {
+    public static void main(String[] args) {
+        DriverExam studentExam = new DriverExam();
+
+        do {
+            System.out.println("Question " + (studentExam.navTracker+1) + " of " + (DriverExam.CORRECT_ANSWERS.length)
+                    + ": " + DriverExam.CORRECT_ANSWERS[studentExam.navTracker]);
+            studentExam.answerQuestion(studentExam.navTracker);
+            studentExam.navMenu();
+        } while (!studentExam.examCompleted);
+        System.out.println("Thank you for completing the exam!");
+    }
 }
